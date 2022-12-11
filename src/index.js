54db43009090dc442a4e5aeff439da97ff287366
Ybednarskyy <y.bednarskyy@gmail.com>
2022-12-11 17:23:12 +0200

    create markup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,51 @@
-const KEY = '31582122';
-const q = 'watermelon';
+import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-fetch(
-  `https://pixabay.com/api/?key=${KEY}-c0bce89aa4307e5dda7d53da1&q=${q}&image_type=photo&pretty=true`
-)
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    data.hits.map(
+const refs = {
+  form: document.querySelector('.search-form'),
+  submit: document.querySelector('button[type="submit"]'),
+  loadMore: document.querySelector('.load-more'),
+  gallery: document.querySelector('.gallery'),
+};
+
+// console.log('submit -> ', refs.submit);
+
+async function getImages(description) {
+  await axios
+    .get(`https://pixabay.com/api/`, {
+      params: {
+        key: '31582122-c0bce89aa4307e5dda7d53da1',
+        q: description,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+        page: '1',
+        per_page: '40',
+      },
+    })
+    .then(response => {
+      const images = response.data.hits;
+      if (!images.length) {
+        return Notify.info(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      }
+      makeImagesList(images);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+console.log(getImages());
+
+function makeImagesList(imalges) {
+  const markup = imalges
+    .map(
       ({
         webformatURL,
         largeImageURL,
@@ -18,20 +55,25 @@ fetch(
         comments,
         downloads,
       }) => {
-        // console.table(
-        //   webformatURL,
-        //   largeImageURL,
-        //   tags,
-        //   likes,
-        //   views,
-        //   comments,
-        //   downloads
-        // );
-
-        const representation = `<img src=${webformatURL} alt=${tags}/>`;
-        const bodyRef = document.querySelector('body');
-        bodyRef.insertAdjacentHTML('beforeend', representation);
+        return `<div class="photo-card">
+      <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+      <div class="info">
+        <p class="info-item">
+          <b>Likes</b> ${likes}
+        </p>
+        <p class="info-item">
+          <b>Views</b> ${views}
+        </p>
+        <p class="info-item">
+          <b>Comments</b> ${comments}
+        </p>
+        <p class="info-item">
+          <b>Downloads</b> ${downloads}
+        </p>
+      </div>
+    </div>`;
       }
-    );
-    // console.log(data.hits);
-  });
+    )
+    .join('');
+  refs.gallery.innerHTML = markup;
+}
